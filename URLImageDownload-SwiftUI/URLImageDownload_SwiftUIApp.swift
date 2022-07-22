9a7f6cde0108a9584c8849d73d5eaaa97fb93dc0
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-21 23:33:27 -0700

    Initial Commit

---
+++ URLImageDownload-SwiftUI/URLImageDownload_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLImageDownload_SwiftUIApp.swift
+//  URLImageDownload-SwiftUI
+//
+//  Created by Mitya Kim on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct URLImageDownload_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
